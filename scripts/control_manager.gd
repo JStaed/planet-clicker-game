c6fb3607bff90671b8a05217e746b96acb6d54cd
Jane Staed <jane.staed@gmail.com>
2026-01-09 22:33:53 -0500

    15 minutes - scripting

--- scripts/control_manager.gd
+++ scripts/control_manager.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 var score: int = 0
-var increment: int = 1
+var score_increment: int = 1
 
 var selected_sprite: int = 0
 
@@ -10,9 +10,25 @@ var unlocked_sprites = [
 	true, false, false, false, false, false, false, false, false, false
 ]
 
+@onready var planet_sprites = [
+	preload("res://planet-sprites/planet00.png"),
+	preload("res://planet-sprites/planet01.png"),
+	preload("res://planet-sprites/planet02.png"),
+	preload("res://planet-sprites/planet03.png"),
+	preload("res://planet-sprites/planet04.png"),
+	preload("res://planet-sprites/planet05.png"),
+	preload("res://planet-sprites/planet06.png"),
+	preload("res://planet-sprites/planet07.png"),
+	preload("res://planet-sprites/planet08.png"),
+	preload("res://planet-sprites/planet09.png")
+]
+
 func _on_click_planet(): # Increments score by increment value when the planet is clicked
-	score += increment
-	$CanvasContainer/Middle/ScoreLabel.text = "Score: " + str(score)
+	if unlocked_sprites[selected_sprite]:
+		score += score_increment
+		$CanvasContainer/Middle/ScoreLabel.text = "Score: " + str(score)
+	else:
+		pass
 
 func _cycle_planet_sprite_forward():
 	selected_sprite = increment_clamped(selected_sprite, 0, 9)
@@ -22,10 +38,13 @@ func _cycle_planet_sprite_backward():
 	selected_sprite = increment_clamped(selected_sprite, 0, 9, -1)
 	print(unlocked_sprites[selected_sprite])
 
-func increment_clamped(base: int, min: int, max: int, increment: int = 1) -> int:
+func increment_clamped(base: int, value_min: int, value_max: int, increment: int = 1) -> int:
 	base += increment
-	if base > max:
-		base = min
-	elif base < min:
-		base = max
+	if base > value_max:
+		base = value_min
+	elif base < value_min:
+		base = value_max
 	return base
+
+func update_planet_sprite(spriteID: int):
+	pass
